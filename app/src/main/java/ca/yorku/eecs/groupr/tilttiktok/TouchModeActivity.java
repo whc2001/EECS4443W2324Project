6dbce4f692de490cbbd96f6a76904821e224f380
whc2001 <ianwang0122@outlook.com>
2024-04-02 18:12:52 -0400

    feat: ViewPager2 scrolling view

--- app/src/main/java/ca/yorku/eecs/groupr/tilttiktok/TouchModeActivity.java
+++ app/src/main/java/ca/yorku/eecs/groupr/tilttiktok/TouchModeActivity.java
@@ -3,17 +3,33 @@ package ca.yorku.eecs.groupr.tilttiktok;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.Surface;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class TouchModeActivity extends Activity {
     final static String MYDEBUG = "MYDEBUG"; // for Log.i messages
+
+    final static int CONTENT_COLOR_RED = Color.argb(255, 204, 102, 102);
+    final static int CONTENT_COLOR_GREEN = Color.argb(255, 102, 204, 102);
+    final static int CONTENT_COLOR_BLUE = Color.argb(255, 102, 102, 204);
+    final int[] pageColors = {CONTENT_COLOR_BLUE, CONTENT_COLOR_RED, CONTENT_COLOR_GREEN, CONTENT_COLOR_BLUE, CONTENT_COLOR_RED};   // Colors for each page, the first and the last is for the infinite scrolling effect
+
+    private int currentPage = 1;    // Start from the second page, which is the first real page
 
     private SensorManager sm;
     private Sensor gyro;
@@ -21,10 +37,97 @@ public class TouchModeActivity extends Activity {
 
     private ExperimentSetup setup;
 
+    private ViewPager2 vpContent;
+    private ImageButton likeButton, commentButton, shareButton;
+
+    private ViewPager2.OnPageChangeCallback onPageChange = new ViewPager2.OnPageChangeCallback() {
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            if (position > currentPage) {
+                Log.i(MYDEBUG, ">>>NEXT>>>");
+            } else if (position < currentPage) {
+                Log.i(MYDEBUG, "<<<PREV<<<");
+            }
+            currentPage = position;
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            super.onPageScrollStateChanged(state);
+            if (state == ViewPager.SCROLL_STATE_IDLE) {
+                // If the user scrolls to the first page, teleport silently to the second last page for infinite scrolling effect
+                // When teleporting temporarily unregister the callback to avoid multiple scroll events
+                if (currentPage == 0) {
+                    vpContent.unregisterOnPageChangeCallback(this);
+                    vpContent.setCurrentItem(pageColors.length - 2, false);
+                    vpContent.registerOnPageChangeCallback(this);
+                    currentPage = pageColors.length - 2;
+                }
+                // Same goes on the other side
+                else if (currentPage == pageColors.length - 1) {
+                    vpContent.unregisterOnPageChangeCallback(this);
+                    vpContent.setCurrentItem(1, false);
+                    vpContent.registerOnPageChangeCallback(this);
+                    currentPage = 1;
+                }
+            }
+        }
+    };
+
+    public void scrollNext() {
+        vpContent.setCurrentItem(currentPage + 1, true);
+    }
+
+    public void scrollPrev() {
+        vpContent.setCurrentItem(currentPage - 1, true);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.touchui);
+
+        vpContent = findViewById(R.id.content_view);
+        likeButton = findViewById(R.id.like_button);
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollPrev();
+            }
+        });
+        commentButton = findViewById(R.id.comment_button);
+        commentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollNext();
+            }
+        });
+
+        // Setup the ViewPager2 with infinite scrolling effect
+        vpContent.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = new View(parent.getContext());
+                view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                return new RecyclerView.ViewHolder(view) {
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                holder.itemView.setBackgroundColor(pageColors[position]);
+            }
+
+            @Override
+            public int getItemCount() {
+                return pageColors.length;
+            }
+        });
+        vpContent.setCurrentItem(currentPage, false);
+        vpContent.registerOnPageChangeCallback(onPageChange);
+
 
         // get experiment setup
         setup = getIntent().getExtras().getParcelable("setup");
@@ -66,6 +169,7 @@ public class TouchModeActivity extends Activity {
             return Configuration.ORIENTATION_LANDSCAPE;
         else return Configuration.ORIENTATION_PORTRAIT;
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -73,6 +177,7 @@ public class TouchModeActivity extends Activity {
             sm.registerListener(tdl, gyro, SensorManager.SENSOR_DELAY_GAME);
         }
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -80,6 +185,7 @@ public class TouchModeActivity extends Activity {
             sm.unregisterListener(tdl);
         }
     }
+
     @Override
     public void onStop() {
         super.onStop();
