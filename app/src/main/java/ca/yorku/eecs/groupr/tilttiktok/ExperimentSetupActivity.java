a3af1860293aca29c40c3d0bc9097d6d6c027c98
Haochen Wang <ianwang0122@outlook.com>
2024-04-06 14:31:21 -0400

    fix: rollback setup screen

--- app/src/main/java/ca/yorku/eecs/groupr/tilttiktok/ExperimentSetupActivity.java
+++ app/src/main/java/ca/yorku/eecs/groupr/tilttiktok/ExperimentSetupActivity.java
@@ -17,10 +17,6 @@ public class ExperimentSetupActivity extends Activity
 
 	Spinner cmbSubjectID, cmbControlMethod, cmbTrials;
 
-	TextView lblCurrentAction;
-
-	ExperimentCoordinator coord;
-
 	// called when the activity is first created
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
@@ -39,27 +35,10 @@ public class ExperimentSetupActivity extends Activity
 		cmbTrials = (Spinner) findViewById(R.id.cmbTrials);
 		cmbTrials.setAdapter(new ArrayAdapter<CharSequence>(this, R.layout.spinnerstyle, TRIALS));
 		cmbTrials.setSelection(TRIALS.length - 1);
-
-		lblCurrentAction = (TextView) findViewById(R.id.lblCurrentAction);
 	}
 
 	// called when the "OK" button is tapped
 	public void btnOK_Click(View view)
-	{
-		// get the selected values from the spinners
-		String subjectID = cmbSubjectID.getSelectedItem().toString();
-		ControlMethod controlMethod = ControlMethod.fromString(cmbControlMethod.getSelectedItem().toString());
-		int trials = Integer.parseInt(cmbTrials.getSelectedItem().toString());
-
-		// construct the bundle and start the experiment activity
-		Bundle b = new Bundle();
-		b.putParcelable("setup", new ExperimentSetup(subjectID, controlMethod, trials));
-		Intent i = new Intent(getApplicationContext(), ExperimentActivity.class);
-		i.putExtras(b);
-		startActivity(i);
-	}
-
-	public void btnOK2_Click(View view)
 	{
 //		// get the selected values from the spinners
 		String subjectID = cmbSubjectID.getSelectedItem().toString();
@@ -69,47 +48,9 @@ public class ExperimentSetupActivity extends Activity
 		// construct the bundle and start the experiment activity
 		Bundle b = new Bundle();
 		b.putParcelable("setup", new ExperimentSetup(subjectID, controlMethod, trials));
-		Intent i = new Intent(getApplicationContext(), TouchModeActivity.class);
+		Intent i = new Intent(getApplicationContext(), controlMethod == ControlMethod.TOUCH ? TouchModeActivity.class : null);
 		i.putExtras(b);
 		startActivity(i);
-//		ExperimentCoordinatorCallback callback = new ExperimentCoordinatorCallback() {
-//			@Override
-//			public void onExperimentStart() {
-//				Log.e("ExperimentCoordinator", "onExperimentStart");
-//			}
-//
-//			@Override
-//			public void onNewTrial(int trial, ExperimentAction action) {
-//				Log.e("ExperimentCoordinator", "onNewTrial: " + trial + " " + action);
-//				lblCurrentAction.setText(action.toString());
-//			}
-//
-//			@Override
-//			public void onIncorrectAction(int trial, ExperimentAction action) {
-//				Log.e("ExperimentCoordinator", "onIncorrectAction: " + trial + " " + action);
-//			}
-//
-//			@Override
-//			public void onExperimentFinished(ExperimentResult result) {
-//				Log.e("ExperimentCoordinator", "onExperimentFinished");
-//				lblCurrentAction.setText("-Experiment Finished-");
-//				for(int i = 0; i < result.getDurationEachTrial().length; i++) {
-//					Log.e("ExperimentCoordinator", "Trial " + i + ": " + result.getDurationEachTrial()[i] + "ms");
-//				}
-//
-//				for(int i = 0; i < result.getIncorrectActionEachTrial().length; i++) {
-//					Log.e("ExperimentCoordinator", "Trial " + i + ": " + result.getIncorrectActionEachTrial()[i] + " incorrect actions");
-//				}
-//
-//				ExperimentResultExporter.write(result);
-//			}
-//		};
-//
-//		ExperimentSetup setup = new ExperimentSetup("99", ControlMethod.TOUCH, 10);
-//
-//		coord = new ExperimentCoordinator(setup, callback);
-//
-//		coord.startExperiment();
 	}
 
 	/** Called when the "Exit" button is pressed. */
@@ -117,40 +58,5 @@ public class ExperimentSetupActivity extends Activity
 	{
 		super.onDestroy(); // cleanup
 		this.finish(); // terminate
-	}
-
-	public void btnPrev_Click(View view)
-	{
-		if(coord != null) {
-			coord.performAction(ExperimentAction.PREVIOUS);
-		}
-	}
-
-	public void btnNext_Click(View view)
-	{
-		if(coord != null) {
-			coord.performAction(ExperimentAction.NEXT);
-		}
-	}
-
-	public void btnLike_Click(View view)
-	{
-		if(coord != null) {
-			coord.performAction(ExperimentAction.LIKE);
-		}
-	}
-
-	public void btnComment_Click(View view)
-	{
-		if(coord != null) {
-			coord.performAction(ExperimentAction.COMMENT);
-		}
-	}
-
-	public void btnShare_Click(View view)
-	{
-		if(coord != null) {
-			coord.performAction(ExperimentAction.SHARE);
-		}
 	}
 }
